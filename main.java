0a8f65484f6f06666e2dfbc71b57016bebf168c1
Technology Rooms <technologyrooms@colsanjose.edu.co>
2024-08-15 14:39:08 -0500

    primer programa

---
+++ main.java
@@ -0,0 +1,9 @@
+class main{
+    public static void main(String args[]){
+        int x = 0;
+        float y= 0.5f;
+        double v= 700000000;
+        boolean b= true;
+        System.out.print("hola mundo");
+    }
+}
